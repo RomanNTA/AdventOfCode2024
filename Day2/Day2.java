23a9485b4ee8f7b71077da225fa530b4ce137b93
RomanNTA <stivi@centrum.cz>
2025-02-17 20:11:30 +0100

    Add files via upload

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -5,74 +5,71 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 public class Day2 {
 
     String FILE_PATH = "src/main/java/cz/sliva/Day2/";
     String FILE_NAME = "input.txt";
-
+    List<String> rows = new ArrayList<>();
     private Integer totalSum = 0;
-    private final Integer[] p1 = new Integer[1000];
-    private final Integer[] p2 = new Integer[1000];
-
-    private Integer position1 = 0;
-    private Integer position2 = 0;
 
     public static void main(String[] args) {
-
-        Day2 t = new Day2();
-
-        if (t.loadFile()) {
-            t.calculate();
-        }
+        (new Day2()).run();
     }
 
-    private void calculate() {
+    public void run(){
+        System.out.printf("\r\nSTART.\r\n\r\n");
+        if (!loadFile()){
+            return;
+        }
+        process();
+        System.out.printf("Result %d \r\n\r\n", this.totalSum);
+    }
 
-        List<Integer> a1 = Arrays.stream(p1).toList();
-        List<Integer> a2 = Arrays.stream(p2).toList();
+    private void process(){
 
-        Map<Integer,Integer> b1 = new HashMap<>(1000);
-        Map<Integer,Integer> b2 = new HashMap<>(1000);
+        rows.forEach(s -> {
+            String[] inp = s.split(" ");
+            Integer[] np = Arrays.stream(inp)
+                    .map(Integer::parseInt)
+                    .toArray(Integer[]::new);
 
-        a1.forEach((a) -> b1.put(a, b1.getOrDefault(a, 0) + 1));
-        a2.forEach((a) -> b2.put(a, b2.getOrDefault(a, 0) + 1));
+            boolean b = true;
+            if (np[0] < np[1]){
+                // pole je vzestupné
+                for (int i = 0; i < np.length-1; i++) {
+                    if (!(np[i+1] - np[i] > 0 && np[i+1] - np[i] <= 3)) {
+                        b = false;
+                        break;
+                    }
+                }
+                this.totalSum += b ? 1 : 0;
 
-        b1.forEach((a,b)-> {
-
-            if (b2.containsKey(a)) {
-                int key = b2.get(a);
-                int value = b2.get(a);
-
-                System.out.printf(" a = %d, b= %d, c = %d, d = %d\r\n", a,b, key, value);
-                totalSum += b * key;
+            } else {
+                // pole je sestupné
+                for (int i = 0; i < np.length-1; i++) {
+                    if (!(np[i] - np[i+1] > 0 && np[i] - np[i+1] <= 3)) {
+                        b = false;
+                        break;
+                    }
+                }
+                this.totalSum += b ? 1 : 0;
             }
         });
-        System.out.printf("Result = %d\r\n\r\n", totalSum);
     }
 
     public boolean loadFile() {
 
-        System.out.println("\r\nSTART\r\n");
-
         Path file = FileSystems.getDefault().getPath(FILE_PATH,FILE_NAME);
         try (Stream<String> lines = Files.lines(file, StandardCharsets.UTF_8)) {
-
-            lines.forEach(s -> {
-                s = s.replace("   ", " ");
-                String[] inp = s.split(" ");
-                p1[position1++] = Integer.parseInt(inp[0]);
-                p2[position2++] = Integer.parseInt(inp[1]);
-            });
-
+            rows = lines.toList();
         } catch (IOException e) {
             System.out.println("Error : Load file.");
-            return false;
+            return false ;
         }
         return true;
     }
